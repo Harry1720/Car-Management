fea6f1b3e5de35918ac897cad56ad670f5d5bfca
Harry1720 <22h1120002@ut.edu.vn>
2025-10-27 17:58:46 +0700

    fix file name slideshow

--- car-management-react/src/components/Slideshow.jsx
+++ car-management-react/src/components/Slideshow.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../assets/css/components/Slideshow.css';
+import '../assets/css/components/slideshow.css';
 
 const Slideshow = () => {
     const [slideIndex, setSlideIndex] = useState(0);
